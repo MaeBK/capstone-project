c6843664a10323bddb99b1e970292d3c4f53eca5
Mae K <maeghanbk@gmail.com>
2022-08-30 23:19:13 -0400

    Needed to add a bit more to aboutme css

--- my-app/src/components/AboutMe.js
+++ my-app/src/components/AboutMe.js
@@ -11,11 +11,13 @@ const AboutMe = () => {
                 </div>
             </div>
             <div  className="col-lg-6 col-xm-12">
+                <div className="about-text">
                 <h1 className="about-heading">Hello there!</h1>
                 <p>
                     I'm Mae Keenan and over the past six months my cohort and I have been a part of a wonderful program called "Road to Hire", and thanks to it this portfolio can be made! 
                     We have learned how to work with HTML, CSS, JavaScript, Java, React, Node.js, and so much more. All of Cohort 10 is excited to be placed with our employee partners, and can't wait to get to work on our careers and paths ahead.
                 </p>
+                </div>
             </div>
         </div>
     </div>
