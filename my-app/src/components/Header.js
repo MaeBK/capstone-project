8230e3e403347ad981b47013b731ab727ddbc57f
Mae K <maeghanbk@gmail.com>
2022-08-30 20:20:51 -0400

    Header is added (no img animation)

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Typed from "react-typed";
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+        <div className="main-info">
+            <h1>Mae Keenan's Portfolio</h1>
+            <Typed
+                className="typed-text"
+                strings={["UI/UX Designer", "Full Stack Developer", "Artist", "Apprentice"]}
+                typeSpeed={40}
+                backSpeed={55}
+                loop
+            />
+            <a href='#' className='btn-main-offer'>Contact Me</a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
